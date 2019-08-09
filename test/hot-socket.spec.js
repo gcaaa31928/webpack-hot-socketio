8f7adc8a7e2345f1a3997d350eb45480b4eccb16
gcaaa31928 <gcaaa31928@gmail.com>
2019-08-10 01:47:48 +0800

    test: added hot socket test

--- test/hot-socket.spec.js
+++ test/hot-socket.spec.js
@@ -2,6 +2,17 @@ const sinon = require('sinon');
 const EventEmitter = require('events');
 const webpackHotSocket = require('../index');
 
+function stats(data) {
+	return {
+		compilation: {
+			name: 'compilation',
+		},
+		toJson: function() {
+			return data;
+		},
+	};
+}
+
 describe('socket', function() {
 	let processUpdate;
 	let socket;
@@ -23,12 +34,74 @@ describe('socket', function() {
 	context('with default options', function() {
 		it('should notify clients when bundle rebuild begins', function(done) {
 			function verify (data) {
-				sinon.assert.match({ action: 'building'}, data)
+				sinon.assert.match({ action: 'building'}, data);
 				done();
 			}
 			socket.on('__webpack_hot_socketio__', verify);
 			compiler.emit('invalid');
 		});
+		it ('should notify clients when bundle is complete', function(done) {
+			function verify (data) {
+				sinon.assert.match('built', data.action);
+				done();
+			}
+			socket.on('__webpack_hot_socketio__', verify);
+			compiler.emit(
+				'done',
+				stats({
+					time: 100,
+					hash: 'redeadfdffd',
+					warnings: false,
+					errors: false,
+					modules: [],
+				})
+			);
+		});
+		it('should notify clients when bundle is complete (multicompiler)', function(done) {
+			function verify(data) {
+				sinon.assert.match('built', data.action);
+				done();
+			}
+			socket.once('__webpack_hot_socketio__', verify);
+			compiler.emit(
+				'done',
+				stats({
+					children: [
+						{
+							time: 100,
+							hash: 'readsdfsdfs',
+							warnings: false,
+							errors: false,
+							modules: [],
+						},
+						{
+							time: 150,
+							hash: 'wefwfwwef',
+							warnings: false,
+							errors: false,
+							modules: [],
+						},
+					],
+				})
+			);
+		});
+		it('should notify new clients about current compilation state', function(done) {
+			function verify(data) {
+				sinon.assert.match('sync', data.action);
+				done();
+			}
+			compiler.emit(
+				'done',
+				stats({
+					time: 100,
+					hash: 'readsdfsdfs',
+					warnings: false,
+					errors: false,
+					modules: [],
+				})
+			);
+			socket.on('__webpack_hot_socketio__', verify);
+			socket.emit('connect');
+		});
 	});
-
 });
